8d5ae957b9d6c3abb9a181d183053356efc20e59
Lucas B <lucas@skyloud.fr>
2024-03-08 16:04:52 +0100

    refacto: reword var name in skills filter function

--- modules/users/users.functions.js
+++ modules/users/users.functions.js
@@ -1,7 +1,7 @@
 import skillsSchema from "../skills/skills.schema.js";
 
 export async function getSkillsFilters(skills) {
-    const skillsIds = skills.map((value) => new RegExp(`${value}`, 'i'));
-    const skillsQuery = await skillsSchema.find({ label: { $in: skillsIds } });
+    const skillsRegex = skills.map((value) => new RegExp(`${value}`, 'i'));
+    const skillsQuery = await skillsSchema.find({ label: { $in: skillsRegex } });
     return { skills: { $in: skillsQuery.map((skill) => skill._id) } };
 }
